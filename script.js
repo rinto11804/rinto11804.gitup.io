241d14164e1cd54d352b22553995cd6ad6963d5a
rinto11804 <70879873+rinto11804@users.noreply.github.com>
2020-09-09 13:57:43 +0530

    re

---
+++ script.js
@@ -0,0 +1,29 @@
+let v=0;
+function my()
+{
+    if(v<10)
+    {
+        v=v+1;
+    }
+    document.getElementById("b").value=v;
+}
+let s=0;
+function my1()
+{
+    if(s<10)
+    {
+        s=s+1;
+    }
+    document.getElementById("c").value=s;
+} 
+
+let z=0;
+function my2()
+{
+    if(z<10)
+    {
+        z=z+1;
+    }
+    document.getElementById("d").value=z;
+} 
+        
